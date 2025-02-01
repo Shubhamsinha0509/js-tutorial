39e6a124bfdb21c5c95cb10b3a0c1fe45e137653
Shubham Sinha <149707578+Shubhamsinha0509@users.noreply.github.com>
2025-02-01 18:25:55 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 2+2 ) // we are using nodejs not browser
+
+// number => 2 to pwr 53
+// bigint
+// string => ""
+// boolean => true/false
+// null
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "shubham")
+console.log(typeof null)  // output -> object
+console.log(typeof undefined) // undefined

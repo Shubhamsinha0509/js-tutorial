c50abeb697dc15edbeaf946e69fd07b02b0c4335
Shubham Sinha <149707578+Shubhamsinha0509@users.noreply.github.com>
2025-02-19 18:38:32 +0000

    changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
 
-console.log(typeof score)
+//console.log(typeof score)
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber) 
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) 
 
 // 33abc is not a number but it shows number in typeof 
 // "33" => 33
@@ -14,5 +14,23 @@ console.log(valueInNumber)
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn) // true
+//console.log(booleanIsLoggedIn) // true
+
+//**************Operations********* 
+
+let value = 7 
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = " shubham"
+
+let str3 = str1+str2
+
+console.log(str3)
+
+console.log("1" + 2)  // 12
+console.log(1 + "2")  // 12
+console.log("1" + 2 + 2)  // 122
+console.log(1 + 2 + "2")  //32
 

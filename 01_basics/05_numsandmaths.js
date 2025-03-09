5bcc4fb31179e81b98dd287396f5a23c301c1e1c
Shubham Sinha <149707578+Shubhamsinha0509@users.noreply.github.com>
2025-03-09 17:19:51 +0000

    math and num

---
+++ 01_basics/05_numsandmaths.js
@@ -0,0 +1,32 @@
+const score = 100
+
+const balance =  new Number(100)
+
+// console.log(score)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(1))
+
+const otherNumber = 234.7896
+
+// console.log(otherNumber.toPrecision(3))
+
+//+++++++++++++Maths+++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))   //4
+// console.log(Math.round(4.6)) //6
+// console.log(Math.ceil(4.2))  //5
+// console.log(Math.floor(4.8))  //4
+// console.log(Math.min(4,3,5,6)) //3
+// console.log(Math.max(4,7,2,9)) //9
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+
+const min = 1
+const max = 6
+
+console.log(Math.floor(Math.random()*(max-min + 1) ) + min)

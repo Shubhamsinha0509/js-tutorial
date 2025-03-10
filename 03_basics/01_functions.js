6c4b8ba53f74e08fe0aea8f0a2fc04dc7007305b
Shubham Sinha <149707578+Shubhamsinha0509@users.noreply.github.com>
2025-03-10 12:43:22 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,26 @@
+function sayMyName (){               
+    console.log("s")
+    console.log("h")
+    console.log("u")
+    console.log("b")
+    console.log("h")
+    console.log("a")
+    console.log("m")
+}
+
+sayMyName()
+
+// function addTwonumbers (number1,number2){  // parameters
+//     console.log(number1 + number2)
+// }
+
+// addTwonumbers(2,4)   // arguments
+
+function addTwonumbers (number1,number2){
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwonumbers(4,5)
+console.log("result",result)

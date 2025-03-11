270b23e2135da2f77d149c6e01b0a0f12e24f863
Shubham Sinha <149707578+Shubhamsinha0509@users.noreply.github.com>
2025-03-11 16:58:09 +0000

    true and false values

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,13 @@
+const userEmail = []
+
+if(userEmail){
+    console.log("Got user email")
+}else {
+    console.log("Don't have user email")         // Got user email
+}
+
+// falsy values 
+// false , null , undefined , BigInt , 0n , 0 , -0 , NaN , ""
+
+// truthy values
+// "0" , 'false' , " " , [] , {} , function (){}
